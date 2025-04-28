e782b0dfd318fd381cee944d5fa0becd532b1920
kristine20 <gyulbekyankristine@gmail.com>
2025-04-28 15:21:26 +0400

    Fix bugs

--- app/components/Company/Company.js
+++ app/components/Company/Company.js
@@ -21,20 +21,23 @@ export default function Company() {
       >
         <div>
           <h2>Компания PUIG ― </h2>
-          <p className="company-text">
-            семейная компания, которой в настоящее время управляет третье
-            поколение семьи Puig. Штаб-квартира Puig располагается в Барселоне.
-            Залог успеха компании ― в умелом развитии модных и парфюмерных
-            брендов, грамотном использовании уникальных концепций и высоком
-            качестве продукции.
-          </p>
-          <p className="company-text">
-            PUIG полностью владеет такими домами моды как: Paco Rabanne,
-            Carolina Herrera, Nina Ricci, Jean Paul Gaultier и Dries Van Noten.
-            А также работает по лицензии с марками: Christian Louboutin, Comme
-            Des Garsons, Antonio Banderas, Shakira и United Colors of Benetton.
-            Продукция компании представлена более чем в 150 странах мира.
-          </p>
+          <div className="counted-colum">
+            <p className="company-text">
+              семейная компания, которой в настоящее время управляет третье
+              поколение семьи Puig. Штаб-квартира Puig располагается в
+              Барселоне. Залог успеха компании ― в умелом развитии модных и
+              парфюмерных брендов, грамотном использовании уникальных концепций
+              и высоком качестве продукции.
+            </p>
+            <p className="company-text">
+              PUIG полностью владеет такими домами моды как: Paco Rabanne,
+              Carolina Herrera, Nina Ricci, Jean Paul Gaultier и Dries Van
+              Noten. А также работает по лицензии с марками: Christian
+              Louboutin, Comme Des Garsons, Antonio Banderas, Shakira и United
+              Colors of Benetton. Продукция компании представлена более чем в
+              150 странах мира.
+            </p>
+          </div>
         </div>
       </motion.div>
       <motion.div
