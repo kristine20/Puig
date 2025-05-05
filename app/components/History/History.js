57beaa11d95b55acf190993b3116064aea28d642
kristine20 <gyulbekyankristine@gmail.com>
2025-05-05 17:37:15 +0400

    Fix bugs

--- app/components/History/History.js
+++ app/components/History/History.js
@@ -1,9 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
-
 import arrow from "../../assets/images/arrow.png";
 import "./History.css";
 
@@ -40,91 +38,77 @@ const items = [
   { year: 2024, content: "Launched new platform" },
 ];
 
-const responsive = {
-  superLargeDesktop: { breakpoint: { max: 4000, min: 1280 }, items: 6 },
-  desktop: { breakpoint: { max: 1280, min: 1024 }, items: 4 },
-  tablet: { breakpoint: { max: 1024, min: 768 }, items: 2 },
-  mobile: { breakpoint: { max: 768, min: 0 }, items: 1 },
-};
-
-const CustomLeftArrow = ({ onClick }) => (
-  <button className="history-button" onClick={onClick}>
-    <Image src={arrow} alt="prev" style={{ transform: "rotate(180deg)" }} />
-  </button>
-);
-
-const CustomRightArrow = ({ onClick }) => (
-  <button className="history-button" onClick={onClick}>
-    <Image src={arrow} alt="next" style={{ marginLeft: "40px" }} />
-  </button>
-);
-
 function History() {
-  const [activeIndex, setActiveIndex] = useState(1);
-  const [visibleItems, setVisibleItems] = useState(4); // Default fallback
+  const [activeIndex, setActiveIndex] = useState(null);
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const nextIndex = (activeIndex + 1) % items.length; // Cycle through items
+      setActiveIndex(nextIndex);
+    }, 5000); // 3 seconds delay before changing index
+
+    return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount or re-render
+  }, [activeIndex]);
 
   useEffect(() => {
-    const updateVisibleItems = () => {
-      const width = window.innerWidth;
-      if (width >= 1280) setVisibleItems(6);
-      else if (width >= 1024) setVisibleItems(4);
-      else if (width >= 768) setVisibleItems(2);
-      else setVisibleItems(1);
-    };
+    const scrollers = document.querySelectorAll(".scroller");
 
-    updateVisibleItems();
-    window.addEventListener("resize", updateVisibleItems);
-    return () => window.removeEventListener("resize", updateVisibleItems);
+    // If a user hasn't opted in for reduced motion, then we add the animation
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      addAnimation();
+    }
+
+    function addAnimation() {
+      scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+        const scrollerInner = scroller.querySelector(".scroller__inner");
+        const scrollerContent = Array.from(scrollerInner.children);
+        setActiveIndex(activeIndex + 1);
+        scrollerContent.forEach((item) => {
+          const duplicatedItem = item.cloneNode(true);
+          duplicatedItem.setAttribute("aria-hidden", true);
+          scrollerInner.appendChild(duplicatedItem);
+        });
+      });
+    }
   }, []);
 
   return (
     <section className="w-fixed pt-2 mb-80 history">
       <h2>История PUIG ―</h2>
-      <Carousel
-        responsive={responsive}
-        arrows
-        autoPlay
-        autoPlaySpeed={30000}
-        infinite
-        customLeftArrow={<CustomLeftArrow />}
-        customRightArrow={<CustomRightArrow />}
-        transitionDuration={500}
-        pauseOnHover={false}
-        containerClass="history-slider-wrapper marquee-wrapper"
-        afterChange={(index) => setActiveIndex(index + 1)}
-      >
-        {items.map((item, index) => {
-          const isActive = index === activeIndex;
-          console.log(activeIndex, "activeIndex");
-          return (
-            <div key={item.year} className={`history-slider-item marquee`}>
-              <div className="circle-wrapper">
-                <svg
-                  className={`circle-svg `}
-                  width="40"
-                  height="40"
-                  viewBox="0 0 40 40"
-                >
-                  <circle
-                    className="circle-path"
-                    cx="20"
-                    cy="20"
-                    r="18"
-                    fill="none"
-                    stroke={isActive ? "#000" : "#000"}
-                    strokeWidth={isActive ? 1 : 0.5}
-                  />
-                </svg>
-                <p className={`year ${isActive ? "active" : ""}`}>
-                  {item.year}
-                </p>
+      <div className="scroller" data-direction="left" data-speed="slow">
+        <div className="scroller__inner">
+          {items.map((item, index) => {
+            const isActive = index === activeIndex;
+            return (
+              <div key={item.year} className={`history-slider-item marquee`}>
+                <div className="circle-wrapper">
+                  <svg
+                    className={`circle-svg `}
+                    width="40"
+                    height="40"
+                    viewBox="0 0 40 40"
+                  >
+                    <circle
+                      className="circle-path"
+                      cx="20"
+                      cy="20"
+                      r="18"
+                      fill="none"
+                      stroke={"#000"}
+                      strokeWidth={isActive ? 1 : 0.5}
+                    />
+                  </svg>
+                  <p className={`year ${isActive ? "active" : ""}`}>
+                    {item.year}
+                  </p>
+                </div>
+                <p className="content">{item.content}</p>
+                {/* <div className="infinite-line"></div> */}
               </div>
-              <p className="content">{item.content}</p>
-              <div className="infinite-line"></div>
-            </div>
-          );
-        })}
-      </Carousel>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
