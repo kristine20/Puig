4005f67ae111cd849721b407fc9a4c7b34e574c3
kristine20 <gyulbekyankristine@gmail.com>
2025-05-06 14:48:38 +0400

    Fix history carousel

--- app/components/History/History.js
+++ app/components/History/History.js
@@ -1,6 +1,137 @@
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+// import Image from "next/image";
+
+// import arrow from "../../assets/images/arrow.png";
+// import "./History.css";
+
+// const items = [
+//   {
+//     year: 1914,
+//     content: "Антонио Пуч Кастелло открывает компанию Antonio Puig S.A.",
+//   },
+//   {
+//     year: 1922,
+//     content: "В продажу поступает Milady, первая испанская губная помада.",
+//   },
+//   {
+//     year: 1940,
+//     content:
+//       "Запуск аромата Agua Lavanda Puig становится символом успеха компании",
+//   },
+//   {
+//     year: 1946,
+//     content: "Строительство новой фабрики и здания офиса в Барселоне.",
+//   },
+//   {
+//     year: 1948,
+//     content:
+//       "Запуск аромата L'Air du Temps. Позже Nina Ricci войдет в состав компании.",
+//   },
+//   {
+//     year: 1950,
+//     content: "Сыновья основателя присоединяются к семейному бизнесу.",
+//   },
+//   { year: 2021, content: "Pivoted to remote work" },
+//   { year: 2022, content: "Redesigned our product" },
+//   { year: 2023, content: "Hit record growth" },
+//   { year: 2024, content: "Launched new platform" },
+// ];
+
+// const responsive = {
+//   superLargeDesktop: { breakpoint: { max: 4000, min: 1280 }, items: 6 },
+//   desktop: { breakpoint: { max: 1280, min: 1024 }, items: 4 },
+//   tablet: { breakpoint: { max: 1024, min: 768 }, items: 2 },
+//   mobile: { breakpoint: { max: 768, min: 0 }, items: 1 },
+// };
+
+// const CustomLeftArrow = ({ onClick }) => (
+//   <button className="history-button" onClick={onClick}>
+//     <Image src={arrow} alt="prev" style={{ transform: "rotate(180deg)" }} />
+//   </button>
+// );
+
+// const CustomRightArrow = ({ onClick }) => (
+//   <button className="history-button" onClick={onClick}>
+//     <Image src={arrow} alt="next" style={{ marginLeft: "40px" }} />
+//   </button>
+// );
+
+// function History() {
+//   const [activeIndex, setActiveIndex] = useState(1);
+//   const [visibleItems, setVisibleItems] = useState(4); // Default fallback
+
+//   useEffect(() => {
+//     const updateVisibleItems = () => {
+//       const width = window.innerWidth;
+//       if (width >= 1280) setVisibleItems(6);
+//       else if (width >= 1024) setVisibleItems(4);
+//       else if (width >= 768) setVisibleItems(2);
+//       else setVisibleItems(1);
+//     };
+
+//     updateVisibleItems();
+//     window.addEventListener("resize", updateVisibleItems);
+//     return () => window.removeEventListener("resize", updateVisibleItems);
+//   }, []);
+
+//   return (
+//     <section className="w-fixed pt-2 mb-80 history">
+//       <h2>История PUIG ―</h2>
+//       <Carousel
+//         responsive={responsive}
+//         arrows
+//         autoPlay
+//         autoPlaySpeed={30000}
+//         infinite
+//         customLeftArrow={<CustomLeftArrow />}
+//         customRightArrow={<CustomRightArrow />}
+//         transitionDuration={500}
+//         pauseOnHover={false}
+//         containerClass="history-slider-wrapper marquee-wrapper"
+//         afterChange={(index) => setActiveIndex(index + 1)}
+//       >
+//         {items.map((item, index) => {
+//           const isActive = index === activeIndex;
+//           console.log(activeIndex, "activeIndex");
+//           return (
+//             <div key={item.year} className={`history-slider-item marquee`}>
+//               <div className="circle-wrapper">
+//                 <svg
+//                   className={`circle-svg `}
+//                   width="40"
+//                   height="40"
+//                   viewBox="0 0 40 40"
+//                 >
+//                   <circle
+//                     className="circle-path"
+//                     cx="20"
+//                     cy="20"
+//                     r="18"
+//                     fill="none"
+//                     stroke={isActive ? "#000" : "#000"}
+//                     strokeWidth={isActive ? 1 : 0.5}
+//                   />
+//                 </svg>
+//                 <p className={`year ${isActive ? "active" : ""}`}>
+//                   {item.year}
+//                 </p>
+//               </div>
+//               <p className="content">{item.content}</p>
+//               <div className="infinite-line"></div>
+//             </div>
+//           );
+//         })}
+//       </Carousel>
+//     </section>
+//   );
+// }
+
+// export default History;
 "use client";
 import React, { useState, useEffect } from "react";
-import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
 import arrow from "../../assets/images/arrow.png";
 import "./History.css";
@@ -39,20 +170,19 @@ const items = [
 ];
 
 function History() {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndex, setActiveIndex] = useState(1);
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      const nextIndex = (activeIndex + 1) % items.length; // Cycle through items
+      const nextIndex = (activeIndex + 1) % items.length;
       setActiveIndex(nextIndex);
-    }, 5000); // 3 seconds delay before changing index
+    }, 5000);
 
-    return () => clearTimeout(timeoutId); // Cleanup timeout on component unmount or re-render
+    return () => clearTimeout(timeoutId);
   }, [activeIndex]);
 
   useEffect(() => {
     const scrollers = document.querySelectorAll(".scroller");
 
-    // If a user hasn't opted in for reduced motion, then we add the animation
     if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
       addAnimation();
     }
@@ -62,28 +192,45 @@ function History() {
         scroller.setAttribute("data-animated", true);
         const scrollerInner = scroller.querySelector(".scroller__inner");
         const scrollerContent = Array.from(scrollerInner.children);
-        setActiveIndex(activeIndex + 1);
-        scrollerContent.forEach((item) => {
-          const duplicatedItem = item.cloneNode(true);
-          duplicatedItem.setAttribute("aria-hidden", true);
-          scrollerInner.appendChild(duplicatedItem);
-        });
+        // scrollerContent.forEach((item) => {
+        //   const duplicatedItem = item.cloneNode(true);
+        //   duplicatedItem.setAttribute("aria-hidden", true);
+        //   scrollerInner.appendChild(duplicatedItem);
+        // });
       });
     }
   }, []);
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev + 1) % items.length);
+  };
+
+  const handlePrevious = () => {
+    setActiveIndex((prev) => (prev - 1 + items.length) % items.length);
+  };
 
   return (
     <section className="w-fixed pt-2 mb-80 history">
       <h2>История PUIG ―</h2>
+      <button onClick={handlePrevious} className="history-button">
+        <Image src={arrow} alt="prev" style={{ transform: "rotate(180deg)" }} />
+      </button>
+
+      <button onClick={handleNext} className="history-button">
+        <Image src={arrow} alt="next" />
+      </button>
+
       <div className="scroller" data-direction="left" data-speed="slow">
         <div className="scroller__inner">
-          {items.map((item, index) => {
-            const isActive = index === activeIndex;
+          {[...items, ...items].map((item, index) => {
+            const isActive = index % items.length === activeIndex;
             return (
-              <div key={item.year} className={`history-slider-item marquee`}>
+              <div
+                key={`${item.year}-${index}`}
+                className="history-slider-item marquee"
+              >
                 <div className="circle-wrapper">
                   <svg
-                    className={`circle-svg `}
+                    className="circle-svg"
                     width="40"
                     height="40"
                     viewBox="0 0 40 40"
@@ -94,7 +241,7 @@ function History() {
                       cy="20"
                       r="18"
                       fill="none"
-                      stroke={"#000"}
+                      stroke="#000"
                       strokeWidth={isActive ? 1 : 0.5}
                     />
                   </svg>
@@ -103,7 +250,6 @@ function History() {
                   </p>
                 </div>
                 <p className="content">{item.content}</p>
-                {/* <div className="infinite-line"></div> */}
               </div>
             );
           })}
