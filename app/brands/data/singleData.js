cdc8f4e9b4cd0239ea64bba1ca2ed6e4e3964b34
kristine20 <gyulbekyankristine@gmail.com>
2025-05-29 17:58:41 +0400

    single  content design

--- app/brands/data/singleData.js
+++ app/brands/data/singleData.js
@@ -4,8 +4,17 @@ import img3 from "../../assets/images/brands/carolina/single/googgirl/3.jpg";
 import img4 from "../../assets/images/brands/carolina/single/googgirl/4.jpg";
 import img5 from "../../assets/images/brands/carolina/single/googgirl/5.jpg";
 import img6 from "../../assets/images/brands/carolina/single/googgirl/6.jpg";
-
 import rightimg from "../../assets/images/brands/carolina/single/googgirl/GOOD GIRL-right.jpg";
+
+import img2_1 from "../../assets/images/brands/carolina/single/goodgirlsupreme/1.jpeg";
+import img2_2 from "../../assets/images/brands/carolina/single/goodgirlsupreme/2.jpeg";
+import img2_3 from "../../assets/images/brands/carolina/single/goodgirlsupreme/3.jpeg";
+import img2_4 from "../../assets/images/brands/carolina/single/goodgirlsupreme/4.jpeg";
+import img2_5 from "../../assets/images/brands/carolina/single/goodgirlsupreme/5.jpeg";
+import img2_6 from "../../assets/images/brands/carolina/single/goodgirlsupreme/6.jpeg";
+import img2_7 from "../../assets/images/brands/carolina/single/goodgirlsupreme/7.jpeg";
+
+import rightimage2 from "../../assets/images/brands/carolina/single/goodgirlsupreme/rightimage.png";
 
 export const singleDetails = {
   carolina: [
@@ -50,6 +59,61 @@ export const singleDetails = {
         },
         {
           text: "Ну а главным символом, желанным объектом становится флакон аромата. Более четырёх лет, гениальная дочь Каролины Эррера разрабатывала дизайн флакона в форме темно-синей туфельки с высокой золотой шпилькой. Именно она разбила сердца миллионов мужчин и женщин по всему миру. Флакон полностью поддерживает образ и характер его обладательниц, такой же чувственный, запоминающийся, женственный и неповторимый!",
+          video: "",
+        },
+      ],
+    },
+
+    {
+      brandTitle: "Carolina Herrera",
+      title: "GOOD GIRL SUPREME",
+      imagegallery: [
+        { id: 1, title: "", src: img2_1 },
+        { id: 2, title: "", src: img2_2 },
+        { id: 3, title: "", src: img2_3 },
+        { id: 4, title: "", src: img2_4 },
+        { id: 5, title: "", src: img2_5 },
+        { id: 6, title: "", src: img2_6 },
+        { id: 7, title: "", src: img2_7 },
+      ],
+      rightImage: [
+        {
+          id: 1,
+          title: "Темные ягоды – Жасмин – Тубероза – Бобы тонка – Ветивер",
+          src: rightimage2,
+        },
+      ],
+      rightContent: [
+        {
+          id: 1,
+          subtext: "Ольфакторное семейство:",
+          text: "Восточный, цветочный",
+        },
+        {
+          id: 2,
+          subtext: "Парфюмер(ы):",
+          text: "Луиза Тёрнер",
+        },
+      ],
+      description: [
+        {
+          text: "Good Girl Eau de Parfum Suprême совершенствует легендарный контраст и заново переосмысляет два культовых для Каролины Эррера цветка: туберозу и жасмин.",
+          video: "",
+        },
+        {
+          text: "Богатая формула Suprême является отражением нашего переменчивого характера. Ее восточное гурманство сочетает в себе изысканные ингредиенты и авангардистскую структуру.",
+          video: "https://www.youtube.com/embed/96F5k6Fxq5M",
+        },
+        {
+          text: "Верхние ягодные ноты подчеркивают наше озорство. «Их гурманский экспрессивный характер возвращает нас в детство» - описывает аромат парфюмер Луиза Тернер.",
+          video: "",
+        },
+        {
+          text: "Чувственность египетского жасмина и яркость туберозы дополняются терпким ягодным оттенком. Впервые этот вид жасмина, известный своей интенсивностью, включен в аромат Carolina Herrera.",
+          video: "",
+        },
+        {
+          text: "Символический дуэт цветов, в базовых аккордах дополнен кремовой текстурой завораживающих бобов тонка. Ноты сердца переплетается с другим ингредиентом, традиционно используемым в мужских ароматах: ветивером, который облагораживает и добавляет современности готовой формуле. «Элегантное звучание напоминает о пузырьках в шампанском и делает аромат интереснее» –говорит парфюмер. Уникальное сочетание ингредиентов привносит современный изыск в ольфакторный мир восточных цветочных ароматов.",
           video: "",
         },
       ],
