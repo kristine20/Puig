40a3f7761f8147152eec5f379bc7f64fbacad4c1
kristine20 <gyulbekyankristine@gmail.com>
2025-06-04 14:39:40 +0400

    Corrections

--- app/auth/login/socialsLogin.js
+++ app/auth/login/socialsLogin.js
@@ -15,7 +15,10 @@ const SocialsLogin = () => {
 
   return (
     <div className={styles.socialButtons}>
-      <button onClick={() => handleLogin("vk")} className={styles.socialButton}>
+      <button
+        onClick={() => handleLogin("VKProvider")}
+        className={styles.socialButton}
+      >
         <Image src={vk} alt="PUIG VK" className={styles.socialIcon} />
       </button>
       <button
@@ -25,7 +28,7 @@ const SocialsLogin = () => {
         <Image src={ok} alt="PUIG OK" className={styles.socialIcon} />
       </button>
       <button
-        onClick={() => handleLogin("yandex")}
+        onClick={() => handleLogin("YandexProvider")}
         className={styles.socialButton}
       >
         <Image src={yandex} alt="PUIG Yandex" className={styles.socialIcon} />
